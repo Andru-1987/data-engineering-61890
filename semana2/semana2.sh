258f4c23b0f33b9d902d248b8aa727a51509dc7f
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-06-10 16:43:32 -0300

    update clases

--- semana2/semana2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "./semana2 to run script"
-LINK=https://docs.google.com/presentation/d/e/2PACX-1vQ5KtWhf9MPj-0eGOHh3n8KEzG97lvhqPUHAX9QYrFiJeBjZu7v7_Ta9Bc1NMNXXg/pub?start=false&loop=false&delayms=3000
