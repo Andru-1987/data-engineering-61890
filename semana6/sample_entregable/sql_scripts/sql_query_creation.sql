ee87b227d39a365e21918f75471b535d2fd727fd
anderson-code-space <anderson.coder.space@gmail.com>
2024-07-30 01:52:01 +0000

    sample

--- semana6/sample_entregable/sql_scripts/sql_query_creation.sql
+++ semana6/sample_entregable/sql_scripts/sql_query_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE stage_api_github(
     id VARCHAR(250),
     name VARCHAR(250),
     full_name VARCHAR(250),
-    private VARCHAR(250),
+    private BOOLEAN,
     html_url VARCHAR(250),
     size VARCHAR(250)
 );
