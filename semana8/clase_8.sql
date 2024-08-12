47da7ad7a7a17ab615e79657a3be86507fdcc5f3
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-08-12 22:06:01 +0000

    microdesafio(features): requirements added plus .env

--- semana8/clase_8.sql
+++ semana8/clase_8.sql
@@ -7,6 +7,20 @@ CREATE TABLE andru_ocatorres_coderhouse.tabla_test_driven (
 	ssn VARCHAR(9)
 )diststyle all;
 
+INSERT INTO
+  andru_ocatorres_coderhouse.tabla_test_driven (name, dob, zip, ssn)
+VALUES
+  ('John Doe', '1980-01-01', 12345, '123456789'),
+  ('Jane Smith', '1990-02-02', 23456, '234567890'),
+  ('Alice Johnson', '1985-03-03', 34567, '345678901'),
+  ('Bob Brown', '1975-04-04', 45678, '456789012'),
+  ('Charlie Davis', '2000-05-05', 56789, '567890123'),
+  ('Diana Evans', '1995-06-06', 67890, '678901234'),
+  ('Frank Green', '1988-07-07', 78901, '789012345'),
+  ('Grace Harris', '1978-08-08', 89012, '890123456'),
+  ('Hank Irving', '1968-09-09', 90123, '901234567'),
+  ('Ivy Johnson', '2002-10-10', 12345, '012345678');
+
 
 -- GENERACION DE UN USUARIO CON SU RESPECTIVO PASSWORD CREATE USER
 
